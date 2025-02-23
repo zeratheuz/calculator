ee835b2b4b0c07b243f696a0dc16f4bd1cea2f83
zeratheuz <zeratheuz@gmail.com>
2025-02-23 17:33:36 -0300

    Add operate function

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-function add(num1 = 0, num2 = 0) {
+function add(num1, num2) {
   return +num1 + +num2
 }
 
-function subtract(num1 = 0, num2 = 0) {
+function subtract(num1, num2) {
   return +num1 - +num2
 }
 
-function multiply(num1 = 0, num2 = 0) {
-  return +num1 * +num2 
-} 
+function multiply(num1, num2) {
+  return +num1 * +num2
+}
 
-function divide(num1 = 0, num2 = 0) {
+function divide(num1, num2) {
   return +num1 / +num2
+}
+
+let num1 = 0
+let num2 = 0
+let operator = ''
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2)
+    case '-':
+      return subtract(num1, num2)
+    case '*':
+      return multiply(num1, num2)
+    case '/':
+      return divide(num1, num2)
+  }
 }
